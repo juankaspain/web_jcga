f9f6888f7e87cf0b23adef9a8e6e1919d904c915
Juan Carlos Garcia Arriero <juanca755@hotmail.com>
2026-02-02 21:57:41 +0100

    feat: add home page with Hero and ImpactStats

---
+++ app/public_root/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/home/Hero"
+import { ImpactStats } from "@/components/home/ImpactStats"
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <ImpactStats />
+    </>
+  )
+}
